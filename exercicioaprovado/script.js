10ebb02d999f025a376b75c7f665085cfaf921e6
JeskaLopes <jessica97cantora@hotmail.com>
2019-04-22 13:56:33 -0300

    adiciona exercicios basicos de javascript

---
+++ exercicioaprovado/script.js
@@ -0,0 +1,12 @@
+let nota1 = parseFloat(prompt('Insira uma nota 1'));
+let nota2 = parseFloat(prompt('Insira a nota 2'));
+let media = (nota1+nota2)/2; 
+console.log(media)
+if(media >= 7)
+if(media==10)
+ alert("Uau! Aprovado com distinção");
+else
+ alert("Parabéns, aprovado! Media "+media);
+else{
+alert("Reprovado!")
+} 
