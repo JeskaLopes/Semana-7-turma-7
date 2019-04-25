9157e3c2724040b96e2b2cba9e40d64b3913da2c
JeskaLopes <jessica97cantora@hotmail.com>
2019-04-25 09:27:16 -0300

    adiciona exercicios de js

--- exerciciodom/script.js
+++ exerciciodom/script.js
@@ -1,8 +1,2 @@
-let titulo = document.querySelector('h1');
-titulo.textContent="Eu sou um titulo"
 
 
-let texto = document.querySelector('.HERO');
-console.log(texto)
-texto.style.color="black";
-texto.style.backgroundColor = '#f12375';
